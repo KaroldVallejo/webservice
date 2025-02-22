d6862ffb925e3aea96afe11168066a44dc4ad332
karold vallejo <karoldvallejo322@gmail.com>
2025-02-22 11:31:41 -0500

    Actualizacion cliente y server

--- cliente.php
+++ cliente.php
@@ -1,3 +1,17 @@
 <?php
-	
+$options = array(
+    'location' => 'http://localhost/webservices/tema2/server.php',
+    'uri'      => 'http://localhost/webservices/tema2/'
+);
+
+$client = new SoapClient(NULL, $options);
+
+$num1 = 4;
+$num2 = 2;
+
+$operaciones = ['+', '-', '*', '/'];
+
+foreach ($operaciones as $operador) {
+    echo "El resultado de $num1 $operador $num2 es: " . $client->calcular($num1, $num2, $operador) . "<br/>";
+}
 ?>
--- server.php
+++ server.php
@@ -1,3 +1,40 @@
 <?php
-	
+
+require('conexion.php');
+
+class serverSoap extends Conexion
+{
+    public function calcular($num1, $num2, $operacion)
+    {
+        return $this->operacionRecursiva($num1, $num2, $operacion);
+    }
+
+    private function operacionRecursiva($num1, $num2, $operacion)
+    {
+        if ($operacion == '+') {
+            return $num1 + $num2;
+        } elseif ($operacion == '-') {
+            return $num1 - $num2;
+        } elseif ($operacion == '*') {
+            return $num1 * $num2;
+        } elseif ($operacion == '/') {
+            return ($num2 != 0) ? $num1 / $num2 : 'Error: División por cero';
+        } else {
+            return 'Operación no válida';
+        }
+    }
+}
+    
+// Crear un nuevo servidor SOAP
+$options = array('uri' => 'http://localhost/webservices/tema2/');
+
+// Instanciar el servidor SOAP
+$server = new SoapServer(NULL, $options);
+
+// Configurar que clase manejará las solicitudes SOAP
+$server->setClass('serverSoap');
+
+// Procesar las solicitudes SOAP
+$server->handle();
+
 ?>
